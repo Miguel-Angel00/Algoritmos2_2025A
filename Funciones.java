f1293dbf8848754ee0e760bd4b661c99b2cc2e7d
Miguel-Angel00 <miguel.villada01@usc.edu.co>
2025-02-25 12:33:00 -0500

    Add files via upload

---
+++ Funciones.java
@@ -0,0 +1,10 @@
+package unidad1;
+
+public class Funciones {
+ /* realizar un juego por consola en el que haya un circulo, que tenga un centro (x,y) dicho circulo tendra un radio, pedirle al usuario a que posicion quiere disparar (x,y)
+ determinar el centro del circulo, pedir al usuario el (x,y) del disparo, y darle un puntaje de acuerdo a la distancia respecto al centro
+ calcular distancia del disparo al centro del circulo
+ crear una funcion llamada "double CalucularDistanciaAlCentro(Xu. Yu)"
+ d = raiz(x^2 + y^2)
+  */
+}
